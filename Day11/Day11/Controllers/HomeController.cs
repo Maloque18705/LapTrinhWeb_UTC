bd6c4aff7bd8a13eb27ffdcecc54be29597a87e1
Maloque18705 <maloque852@gmail.com>
2025-11-07 11:20:06 +0700

    Upload ThucHanh01

---
+++ Day11/Day11/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Day11.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MyForm()
+        {
+            return View();
+        }
+
+        public IActionResult ColorPicker()
+        {
+            return View();
+        }
+
+        public IActionResult MyCart()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
