4a3c9a9d92e2b8a95fee5b41610202b9caafb063
Maloque18705 <maloque852@gmail.com>
2025-11-15 21:24:18 +0700

    Update for Lab05

---
+++ ThucHanh02/ThucHanh02/Controllers/LearnerController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ThucHanh02.Data;
+using ThucHanh02.Models;
+namespace ThucHanh02.Controllers
+{
+    public class LearnerController : Controller
+    {
+        private SchoolContext db;
+        public LearnerController(SchoolContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var learners = db.Learners.Include(m => Major).ToList();
+            return View(learners);
+        }
+
+        public IActionResult Create()
+        {
+            var majors = new List<SelectListItem>();
+            foreach (var item in db.Majors)
+            {
+                majors.Add(new SelectListItem { Text = item.MajorName, Value = item.MajorID.ToString() });
+            }
+            ViewBag.MajorID = majors;
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FirstMidName, LastName, MajorID, EnrollmentDate")] Learner learner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Learners.Add(learner);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View();
+        }
+
+    }
+}
