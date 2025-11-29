972e6df03d1585cf59a4c0099d1330182bd5a9bf
Maloque18705 <maloque852@gmail.com>
2025-11-30 00:27:10 +0700

    Add QLBongDa

---
+++ QLBongDa/QLBongDa/Models/Cau5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QLBongDa.Models;
+
+public partial class Cau5
+{
+    public string? CauLacBoId { get; set; }
+
+    public string? TenClb { get; set; }
+
+    public string? HoVaTen { get; set; }
+
+    public string? TenSan { get; set; }
+
+    public DateTime? NgayThiDau { get; set; }
+
+    public int? ThoiDiemGhiBan { get; set; }
+}
